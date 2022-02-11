363e1bbea0877634bd475515feac818e6817af07
Haitam Skhairi <haitam.skhairi@gmail.com>
2022-02-11 20:04:01 +0100

    Set Animation in The Pricing Section width JaveScript

--- main.js
+++ main.js
@@ -1,19 +1,55 @@
-// Start Scrolling Top
+// Scrolling Top
 let btn = document.querySelector(".scrolling-button");
 
-window.onscroll = () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY >= 300) {
     btn.style.display = "block";
   } else {
     btn.style.display = "none";
   }
-};
-
-btn.onclick = () => {
+});
+btn.addEventListener("click", () => {
   window.scrollTo({
     left: 0,
     top: 0,
     behavior: "smooth",
   });
-};
-// End Scrolling Top
+});
+
+// Animation Skills
+let skkilsSection = document.querySelector(".skills");
+let spansProgress = document.querySelectorAll(".skills .prog span");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= skkilsSection.offsetTop - 300) {
+    spansProgress.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+});
+
+// Animation Pricing
+let pricingSection = document.querySelector(".pricing");
+let nums = document.querySelectorAll(".pricing .box .num");
+let started = false;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= pricingSection.offsetTop) {
+    if (!started) {
+      nums.forEach((tt) => {
+        countUp(tt);
+      });
+    }
+    started = true;
+  }
+});
+
+function countUp(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 1000 / goal);
+}
